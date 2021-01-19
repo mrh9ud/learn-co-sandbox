f32873e0142901655747aadfd99872cd5026d978
Matthew Heavner <mrh9ud@virginia.edu>
2021-01-19 18:47:22 +0000

    Automatically backed up by Learn

---
+++ learning-comments.rb
@@ -0,0 +1,17 @@
+# Perform a constant expression evaluation
+3
+# Assign constant 3 to bare-word variable triangle_sides
+triangle_sides = 3
+
+# From the Three Dog Night song: "Joy to the World (Jeremiah was a Bullfrog)"
+puts "Joy to the world"
+puts "All the boys and girls"
+#puts "Joy to the fishes in the deep blue sea"
+puts "Joy to you and me"
+
+name = "Byron"
+# name = "Luca"
+
+puts "We're sorry, but per health inspector's rules, #{name} is not allowed in the store."
+
+#=> We're sorry, but per health inspetor's rules, Luca is not allowed in the store.
---
+++ methodsandarguments.rb
@@ -0,0 +1,18 @@
+#def greeting_a_person(name)
+#  puts "Hello #{name}"
+#end
+
+#greeting_a_person("Matt")
+
+def greeting_programmer(name, language = "computer")
+  puts "Hello, #{name}. We heard you are a great #{language} programmer."
+end
+
+greeting_programmer("Maria")
+greeting_programmer("Steven", "Elixir")
+
+#def greeting(name = 'neighbor')
+#  puts "Hello, #{name}!"
+#end
+
+#greeting("Matt")
